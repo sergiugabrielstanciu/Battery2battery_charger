b8f27b2c38957f554689dc8d2d4c38fdb512971b
sergiustanciu <sergiugabrielstanciu@gmail.com>
2021-04-08 14:32:59 +0300

    Included libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_Sensor.h>
+#include <RFM69.h>
+#include <RFM69_ATC.h>
+#include <RFM69_OTA.h>
+#include <Adafruit_SleepyDog.h>
+#include <avr/power.h>
 
+#define led_pin    6 // On Trinket or Gemma, suggest changing this to 1
+#define led_number 1 // Popular NeoPixel ring size
+#define DELAYVAL   500 // Time (in milliseconds) to pause between pixels
+
+Adafruit_NeoPixel WS_led(led_number, led_pin, NEO_GRB + NEO_KHZ800);
+Adafruit_INA219 ina219;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  WS_led.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
